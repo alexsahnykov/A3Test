f260618054f395fd49e0c9b3f0f7d460b29f73f0
Alexander <alexsahnykov@gmail.com>
2018-09-13 18:27:47 +0300

    MVC / Stroboard

---
+++ A3Test/Controller/UsersTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  UsersTableViewController.swift
+//  A3Test
+//
+//  Created by Александр Сахнюков on 13/09/2018.
+//  Copyright © 2018 Александр Сахнюков. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    //  Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) 
+
+        
+
+        return cell
+    }
+    
+
+
+}
---
+++ A3Test/View/PhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoTableViewCell.swift
+//  A3Test
+//
+//  Created by Александр Сахнюков on 13/09/2018.
+//  Copyright © 2018 Александр Сахнюков. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
