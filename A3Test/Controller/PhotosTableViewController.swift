89e8d986d795df582f68592d59edadf414dea199
Alexander <alexsahnykov@gmail.com>
2018-09-16 19:26:14 +0300

    some refactoring

--- A3Test/Controller/PhotosTableViewController.swift
+++ A3Test/Controller/PhotosTableViewController.swift
@@ -15,6 +15,8 @@ class PhotosTableViewController: UITableViewController {
     var totalAlbums:[Album] = []
     var firsAlbum = 0
     var cache = NSCache<NSString,AnyObject>()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -35,19 +37,32 @@ class PhotosTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell", for: indexPath) as! PhotoTableViewCell
         let cellItem = photos[indexPath.row]
-        if let imageURL = cache.object(forKey: cellItem.url as NSString) as? URL {
-            let data = try? Data (contentsOf: imageURL)
-            cell.photoView.image =  UIImage(data: data!)
-            print(imageURL)
-        }  else {
-        cell.photoTitleLable.text = cellItem.title
-        cell.photoView.downloaded(from: cellItem.url)
-            self.cache.setObject(cell.photoView.downloaded(from: cellItem.url) as AnyObject, forKey: cellItem.url as NSString)
-            print(cell.photoView.downloaded(from: cellItem.url))
+        
+        
+        if let imageURL = cache.object(forKey: cellItem.url as NSString)  {
+           
+            cell.photoView.image =  imageURL as? UIImage
+            print("in cashe")
+        }  else  {
+        
+            cell.photoTitleLable.text = cellItem.title
+            let url = URL(string: cellItem.url)
+            networkManager.shared.getData(url: url!) { (data) in
+            let image = UIImage(data: data as! Data)
+            DispatchQueue.main.async  {
+                cell.photoView.image = image}
+                
+            self.cache.setObject(image as AnyObject, forKey: cellItem.url as NSString)
+                print(image)
+                print("out cashe")
+        }
         }
         return cell
     }
-   
+        
+    
+    
+    
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let lastitem = photos.count - 1
         if indexPath.row == lastitem {
@@ -65,22 +80,3 @@ class PhotosTableViewController: UITableViewController {
 }
 
 
-extension UIImageView {
-    func downloaded(from url: URL) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() {
-                self.image = image
-            }
-            }.resume()
-    }
-    func downloaded(from link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url)
-    }
-}
--- A3Test/Manager/NetworkManager.swift
+++ A3Test/Manager/NetworkManager.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import UIKit
+
+
 class networkManager {
     private init() {}
     
@@ -70,6 +73,7 @@ class getDataNetworkService {
             }
         }
     }
+  
 
 
 }
